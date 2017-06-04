1900115a7c233913251dea4c0f02c1fc22d10f9e
Troy Center <troycenter1@gmail.com>
2017-06-04 11:06:55 -0700

    got buttons working...

--- js/contactcard.js
+++ js/contactcard.js
@@ -4,29 +4,76 @@
 //==================================================================//
 
 "use strict";
-    $(document).on("click","button"(function(){ 
-            $("#contactcards:last").append("<tr></tr>");
-            $("#contactcards:last").append("<td>"+document.getElementById("firstname").value+"</td>");
-            $("#contactcards:last").append("<td>"+document.getElementById("lastname").value+"</td>");
-            $("#contactcards:last").append("<td>"+document.getElementById("email").value+"</td>");
-            $("#contactcards:last").append("<td>"+document.getElementById("phone").value+"</td>");
-            // $("#userlist tr:last").after.append("</tr>");
-            // alert("Added User");  
-            var firstname = document.getElementById("firstname").value
-            var lastname = document.getElementById("lastname").value
-            success([firstname+" "+lastname]);        
-        }));    
-    function formChanged(){
-        var firstname = document.getElementById("firstname").value;
-            // console.log(firstname);
-        var lastname = document.getElementById("lastname").value;
-            // console.log(lastname);
-        var email = document.getElementById("email").value;
-            // console.log(email);
-        var phone = document.getElementById("phone").value;
-            // console.log(phone);
-    }
-    function success(output){
-        alert("User Added: "+output);
-        $("#contactform").reset();
-    }
+    $(document).ready(function(){
+
+
+        $("#hidebutton").click(function(){
+            alert("hide button clicked")
+            //$("tr").hide();
+        });
+
+        $("#addcontact").click(function(){
+            alert("add button clicked")
+            //$("tr").hide();
+        });
+
+        $(document).on("click","button"(function(){ 
+                $("#contactcards:last").append("<tr></tr>");
+                $("#contactcards:last").append("<td>"+document.getElementById("firstname").value+"</td>");
+                $("#contactcards:last").append("<td>"+document.getElementById("lastname").value+"</td>");
+                $("#contactcards:last").append("<td>"+document.getElementById("email").value+"</td>");
+                $("#contactcards:last").append("<td>"+document.getElementById("phone").value+"</td>");
+                // $("#userlist tr:last").after.append("</tr>");
+                // alert("Added User");  
+                var firstname = document.getElementById("firstname").value
+                var lastname = document.getElementById("lastname").value
+                success([firstname+" "+lastname]);        
+        }));
+
+
+        $("#description").on("click",function(){
+            $("#firstname", "#lastname", "#description").fadeOut(500),
+            $(this).fadeIn(500)
+        });
+
+        // $(".card").on({
+        //     mousenter: function(){
+        //         $(this).fadeOut(500)//,function(){
+        //   //      var srcimg = $(this).;
+        //   //      var altsrcimg = $(this).;
+        //   //      });
+        //     },
+        //     mouseleave: function(){
+        //         $(this).fadeIn(500)//,function(){
+        //   //      var srcimg = $(this).;
+        //   //      var altsrcimg = $(this).;
+        // //        });          
+        //     }});
+            
+    //==================================================================//
+    //  if form changes, or onKeyUp, update variables.                  //
+    //==================================================================//
+
+        function formChanged(){
+            var firstname = document.getElementById("firstname").value;
+                // console.log(firstname);
+            var lastname = document.getElementById("lastname").value;
+                // console.log(lastname);
+            var description = document.getElementById("description").value;
+                // console.log(email);
+        }
+
+    //==================================================================//
+    //  Created: INSERTDATE by Troy Center, troycenter1@gmail.com.      //
+    //  INSERT DESCRIPTION                                              //
+    //==================================================================//
+        
+        function success(output){
+            alert("User Added: "+output);
+            $("#contactform").reset();
+        }
+        
+
+
+
+    });
