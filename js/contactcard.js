7d358d550c56826fc128b7aeb3077b168ddac4fa
Troy Center <troycenter1@gmail.com>
2017-06-04 10:28:48 -0700

    positioned with css and fonts added.

--- js/contactcard.js
+++ js/contactcard.js
@@ -5,11 +5,11 @@
 
 "use strict";
     $(document).on("click","button"(function(){ 
-            $("#userlist tbody:last").append("<tr></tr>");
-            $("#userlist tr:last").append("<td>"+document.getElementById("firstname").value+"</td>");
-            $("#userlist tr:last").append("<td>"+document.getElementById("lastname").value+"</td>");
-            $("#userlist tr:last").append("<td>"+document.getElementById("email").value+"</td>");
-            $("#userlist tr:last").append("<td>"+document.getElementById("phone").value+"</td>");
+            $("#contactcards:last").append("<tr></tr>");
+            $("#contactcards:last").append("<td>"+document.getElementById("firstname").value+"</td>");
+            $("#contactcards:last").append("<td>"+document.getElementById("lastname").value+"</td>");
+            $("#contactcards:last").append("<td>"+document.getElementById("email").value+"</td>");
+            $("#contactcards:last").append("<td>"+document.getElementById("phone").value+"</td>");
             // $("#userlist tr:last").after.append("</tr>");
             // alert("Added User");  
             var firstname = document.getElementById("firstname").value
