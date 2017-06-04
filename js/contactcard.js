44c565dd6ae55e18192009ef2470aa084967ee58
Troy Center <troycenter1@gmail.com>
2017-06-04 13:47:34 -0700

    Just got the display tiles working...

--- js/contactcard.js
+++ js/contactcard.js
@@ -3,77 +3,35 @@
 //  INSERT DESCRIPTION                                              //
 //==================================================================//
 
+
 "use strict";
-    $(document).ready(function(){
+$(document).ready(function(){
 
-
-        $("#hidebutton").click(function(){
-            alert("hide button clicked")
-            //$("tr").hide();
+        $(document).on("click",".namecard",function(){
+            $(".namecard").css("display", "none");
+            $(".descriptioncard").css("display", "block")
+        });
+        $(document).on("click",".descriptioncard",function(){
+            $(".namecard").css("display", "block");
+            $(".descriptioncard").css("display", "none")            
         });
 
-        $("#addcontact").click(function(){
-            alert("add button clicked")
-            //$("tr").hide();
-        });
+//======================================================================//
+//  This is the changerhider. When the card is clicked, it will         //
+//  fade to black, change text, and fade back in. It will revert when.  //
+//======================================================================//
 
-        $(document).on("click","button"(function(){ 
-                $("#contactcards:last").append("<tr></tr>");
-                $("#contactcards:last").append("<td>"+document.getElementById("firstname").value+"</td>");
-                $("#contactcards:last").append("<td>"+document.getElementById("lastname").value+"</td>");
-                $("#contactcards:last").append("<td>"+document.getElementById("email").value+"</td>");
-                $("#contactcards:last").append("<td>"+document.getElementById("phone").value+"</td>");
-                // $("#userlist tr:last").after.append("</tr>");
-                // alert("Added User");  
-                var firstname = document.getElementById("firstname").value
-                var lastname = document.getElementById("lastname").value
-                success([firstname+" "+lastname]);        
-        }));
+    $(document).on("click","#addcontact",function(){ 
 
-
-        $("#description").on("click",function(){
-            $("#firstname", "#lastname", "#description").fadeOut(500),
-            $(this).fadeIn(500)
-        });
-
-        // $(".card").on({
-        //     mousenter: function(){
-        //         $(this).fadeOut(500)//,function(){
-        //   //      var srcimg = $(this).;
-        //   //      var altsrcimg = $(this).;
-        //   //      });
-        //     },
-        //     mouseleave: function(){
-        //         $(this).fadeIn(500)//,function(){
-        //   //      var srcimg = $(this).;
-        //   //      var altsrcimg = $(this).;
-        // //        });          
-        //     }});
-            
-    //==================================================================//
-    //  if form changes, or onKeyUp, update variables.                  //
-    //==================================================================//
-
-        function formChanged(){
-            var firstname = document.getElementById("firstname").value;
-                // console.log(firstname);
-            var lastname = document.getElementById("lastname").value;
-                // console.log(lastname);
-            var description = document.getElementById("description").value;
-                // console.log(email);
-        }
-
-    //==================================================================//
-    //  Created: INSERTDATE by Troy Center, troycenter1@gmail.com.      //
-    //  INSERT DESCRIPTION                                              //
-    //==================================================================//
-        
-        function success(output){
-            alert("User Added: "+output);
-            $("#contactform").reset();
-        }
-        
-
-
+            $("tbody:last").append("<tr class='namecard'>");            
+                $("tr:last").append("<td class='name'>"+document.getElementById("firstname").value+"</td>");
+                $("tr:last").append("<td class='descriptionplaceholder'>'Hover here for description'</td>");
+                $("tr:last").append("</tr>");
+            $("tbody:last").append("<tr class='descriptioncard'>");
+                $("tr:last").append("<td class 'description'>"+document.getElementById("description").value+"</td>");
+                $("tr:last").append("</tr>");
+            $(".descriptioncard").css("display", "none");
 
     });
+            
+});
