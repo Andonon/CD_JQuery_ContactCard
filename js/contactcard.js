18d35f69ce02f59f926c735e3fa03162cb937891
Troy Center <troycenter1@gmail.com>
2017-06-04 10:09:57 -0700

    finished form fields and working on css...

--- js/contactcard.js
+++ js/contactcard.js
@@ -1,5 +1,32 @@
+//==================================================================//
+//  Created: INSERTDATE by Troy Center, troycenter1@gmail.com.      //
+//  INSERT DESCRIPTION                                              //
+//==================================================================//
+
 "use strict";
-    $(document).ready(function(){ 
-        
-    });    
-    
+    $(document).on("click","button"(function(){ 
+            $("#userlist tbody:last").append("<tr></tr>");
+            $("#userlist tr:last").append("<td>"+document.getElementById("firstname").value+"</td>");
+            $("#userlist tr:last").append("<td>"+document.getElementById("lastname").value+"</td>");
+            $("#userlist tr:last").append("<td>"+document.getElementById("email").value+"</td>");
+            $("#userlist tr:last").append("<td>"+document.getElementById("phone").value+"</td>");
+            // $("#userlist tr:last").after.append("</tr>");
+            // alert("Added User");  
+            var firstname = document.getElementById("firstname").value
+            var lastname = document.getElementById("lastname").value
+            success([firstname+" "+lastname]);        
+        }));    
+    function formChanged(){
+        var firstname = document.getElementById("firstname").value;
+            // console.log(firstname);
+        var lastname = document.getElementById("lastname").value;
+            // console.log(lastname);
+        var email = document.getElementById("email").value;
+            // console.log(email);
+        var phone = document.getElementById("phone").value;
+            // console.log(phone);
+    }
+    function success(output){
+        alert("User Added: "+output);
+        $("#contactform").reset();
+    }
